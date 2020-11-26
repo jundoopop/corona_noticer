3ad4c64b9c3f5c756f36a1ee1c68446c2aa7b84d
jundoopop <jundolpop@gmail.com>
2020-11-26 23:41:06 +0900

    challenge for list ordering

--- cases/views.py
+++ cases/views.py
@@ -1,24 +1,26 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.views import generic
+# from django.shortcuts import render
+# from django.http import HttpResponse
+from django.views.generic import ListView
 from .models import SumCorona, DailyCorona
 
 
 # Create your views here.
 
-class DailyPageView(generic.ListView):
+class DailyPageView(ListView):
     template_name = 'cases/daily.html'
     ordering = '-created_at'
     paginate_by = 10
 
-    def get_queryset(self):
-        return DailyCorona.objects.all()
+    def get_ordering(self):
+        ordering = self.request.GET.get('ordering', '-created_at')
+        return ordering
 
 
-class SumPageView(generic.ListView):
+class SumPageView(ListView):
     template_name = 'cases/sum.html'
-    ordering = '-created_at'
+    ordering = 'created_at'
     paginate_by = 10
 
-    def get_queryset(self):
-        return SumCorona.objects.all()
+    def get_ordering(self):
+        ordering = self.request.GET.get('ordering', '-created_at')
+        return ordering
