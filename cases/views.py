0fdeb9229165a1070bd07bc745e724c37304d22b
jundoopop <jundolpop@gmail.com>
2020-12-07 01:26:55 +0900

    challenge for list ordering - 4

--- cases/views.py
+++ cases/views.py
@@ -7,18 +7,24 @@ from .models import SumCorona, DailyCorona
 # Create your views here.
 
 class DailyPageView(ListView):
+    model = DailyCorona
     template_name = 'cases/daily.html'
     ordering = '-created_at'
     paginate_by = 10
 
+    """
     def get_queryset(self):
         return SumCorona.objects.order_by(self, '-created_at')
+    """
 
 
 class SumPageView(ListView):
+    model = SumCorona
     template_name = 'cases/sum.html'
-    ordering = 'created_at'
+    ordering = '-created_at'
     paginate_by = 10
 
+    """
     def get_queryset(self):
         return SumCorona.objects.order_by(self, '-created_at')
+    """
