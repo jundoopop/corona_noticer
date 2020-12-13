abfeb38b7cf1e1bc46ffdb066441c201b9e1fa20
jundoopop <jundolpop@gmail.com>
2020-12-13 16:52:48 +0900

    fixed setting error

--- corona_notice/settings.py
+++ corona_notice/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'scraping.apps.ScrapingConfig',
     'cases.apps.CoronaConfig',
 ]
 
